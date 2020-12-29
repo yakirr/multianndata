45decb44a46d1cc48390af6886bafb85b0b644f1
Yakir Reshef <yr073@cn002.research.partners.org>
2020-12-29 09:33:40 -0500

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="multianndata", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Yakir Reshef, Laurie Rumker",
     author_email="yreshef@broadinstitute.org",
     description="Multi-sample version of AnnData",
